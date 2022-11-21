165f6c1c800f95b855e07712e76029b13cbccbfc
Zuhair Ahmed <78954298+ZuhairAhmed0@users.noreply.github.com>
2022-11-21 12:25:03 +0200

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,54 +1,42 @@
-#include "main.h"
-
-/**
- * parser - print a formated string.
- *
- * @format: string
- * @f_list: funcations
- * @arg_list: argument
- *
- * Return: Total
- */
-int parser(const char *format, convert_to f_list[], va_list arg_list)
+int parser(const char *format, conver_t f_list[], va_list arg_list)
 {
-	int i, j, val, done;
+	int i, j, r_val, done;
 
 	done = 0;
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)/* Iterates through the main str*/
 	{
-		if (format[i] == '%')
+		if (format[i] == '%') /*Checks for format specifiers*/
 		{
+			/*Iterates through struct to find the right func*/
 			for (j = 0; f_list[j].sym != NULL; j++)
 			{
 				if (format[i + 1] == f_list[j].sym[0])
 				{
-					val = f_list[j].f(arg_list);
-					if (val == -1)
+					r_val = f_list[j].f(arg_list);
+					if (r_val == -1)
 						return (-1);
-					done += val;
+					done += r_val;
 					break;
 				}
 			}
-
 			if (f_list[j].sym == NULL && format[i + 1] != ' ')
 			{
 				if (format[i + 1] != '\0')
 				{
 					_putchar(format[i]);
 					_putchar(format[i + 1]);
-					done = done + 2;
+					done = printed_chars + 2;
 				}
 				else
 					return (-1);
 			}
-			i = i + 1;
+			i = i + 1; /*Updating i to skip format symbols*/
 		}
 		else
 		{
-			_putchar(format[i]);
+			_putchar(format[i]); /*call the write function*/
 			done++;
 		}
 	}
-
 	return (done);
 }
